1b70feb8ea2522e9ada4ba16ee42f8f9fe77c564
Ed Ward <ed@lostcoastdev.com>
2021-03-10 08:55:15 -0800

    capify

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,39 +1,80 @@
+# frozen_string_literal: true
+
 # config valid for current version and patch releases of Capistrano
-lock "~> 3.16.0"
 
-set :application, "my_app_name"
-set :repo_url, "git@example.com:me/my_repo.git"
+lock '~> 3.16.0'
 
-# Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+server '64.227.3.155', port: 22, roles: %i[web app db], primary: true
 
-# Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, "/var/www/my_app_name"
+set :rbenv_type, :user
+set :rbenv_ruby, '3.0.0'
+set :rbenv_prefix,
+    "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
+set :rbenv_roles, :all
 
-# Default value for :format is :airbrussh.
-# set :format, :airbrussh
+set :application, 'uppy'
+set :repo_url, 'git@lostcoastdev:lostcoastdev/uppy.git'
+set :user, 'edward'
+set :branch, 'main'
 
-# You can configure the Airbrussh format using :format_options.
-# These are the defaults.
-# set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
+set :init_system, :systemd
+set :puma_threads, [4, 16]
+set :puma_workers, 0
 
-# Default value for :pty is false
-# set :pty, true
+set :pty, false
+set :use_sudo, false
+set :stage, :production
+set :deploy_via, :remote_cache
+set :deploy_to, "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
+set :puma_bind, "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
+set :puma_state, "#{shared_path}/tmp/pids/puma.state"
+set :puma_pid, "#{shared_path}/tmp/pids/puma.pid"
+set :puma_access_log, "#{release_path}/log/puma.error.log"
+set :puma_error_log, "#{release_path}/log/puma.access.log"
+set :ssh_options,
+    { forward_agent: false, user: 'edward', keys: '/Users/edward/.ssh/id_rsa_lostcoastdev', auth_methods: %w[publickey] }
+set :puma_preload_app, true
+set :puma_worker_timeout, nil
+set :puma_init_active_record, false # Change to true if using ActiveRecord
 
-# Default value for :linked_files is []
-# append :linked_files, "config/database.yml"
+set :log_level, :info
+set :keep_releases, 5
 
-# Default value for linked_dirs is []
-# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
+set :linked_files, %w[config/master.key config/database.yml]
 
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+namespace :puma do
+  desc 'Create Directories for Puma Pids and Socket'
+  task :make_dirs do
+    on roles(:app) do
+      execute "mkdir #{shared_path}/tmp/sockets -p"
+      execute "mkdir #{shared_path}/tmp/pids -p"
+    end
+  end
 
-# Default value for local_user is ENV['USER']
-# set :local_user, -> { `git config user.name`.chomp }
+  before :start, :make_dirs
+end
 
-# Default value for keep_releases is 5
-# set :keep_releases, 5
+namespace :deploy do
+  desc 'Make sure local git is in sync with remote.'
+  task :check_revision do
+    on roles(:app) do
+      unless `git rev-parse HEAD` == `git rev-parse origin/main`
+        puts 'WARNING: HEAD is not the same as origin/main'
+        puts 'Run `git push` to sync changes.'
+        exit
+      end
+    end
+  end
 
-# Uncomment the following to require manually verifying the host key before first deploy.
-# set :ssh_options, verify_host_key: :secure
+  desc 'Initial Deploy'
+  task :initial do
+    on roles(:app) do
+      before 'deploy:restart', 'puma:start'
+      invoke 'deploy'
+    end
+  end
+
+  before :starting, :check_revision
+  after :finishing, :compile_assets
+  after :finishing, :cleanup
+end
